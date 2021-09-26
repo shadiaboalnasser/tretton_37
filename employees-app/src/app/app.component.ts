293f291c951e007ad50b46cdabfa630ee6598e17
shadiaboalnasser <shadi21985@gmail.com>
2021-09-26 10:34:33 +0200

    Clean up unused files

--- employees-app/src/app/app.component.ts
+++ employees-app/src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `<app-header></app-header>
-            <app-employees></app-employees>`,
-  styleUrls: ['./app.component.sass']
+            <app-employees></app-employees>`
 })
 export class AppComponent {
 }
